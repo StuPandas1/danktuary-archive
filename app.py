64836d468557bc5600fdcd57a5c0d690ec8dc7c1
Your Name <you@example.com>
2026-06-28 08:56:38 -0400

    undo streaming nonsense

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ pg = st.navigation(
         st.Page("pages/landing.py", title="Dashboard", icon="💀", default=True),
         st.Page("pages/tools.py", title="Useful Tools", icon="🛠️"),
         st.Page("pages/explore.py", title="Explore the Archive", icon="🔍"),
-        st.Page("pages/listen.py", title="Listen", icon="🎧"),
+        # st.Page("pages/listen.py", title="Listen", icon="🎧"),
     ],
     position="hidden"
 )
