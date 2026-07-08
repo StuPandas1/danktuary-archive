5b7f9091da497eea7b98beeac76ea55329425d6f
Your Name <you@example.com>
2026-07-08 19:01:51 -0400

    login bug fix chase

--- pages/listen.py
+++ pages/listen.py
@@ -63,7 +63,7 @@ else:
             login_tab, signup_tab = st.tabs(["Log In", "Create Account"])
 
             with login_tab:
-                name, auth_status, username = authenticator.login("Login", "main")
+                authenticator.login(location="main")
                 st.write("Authentication:", st.session_state.get("authentication_status"))
                 st.write("Name:", st.session_state.get("name"))
                 st.write("Username:", st.session_state.get("username"))
