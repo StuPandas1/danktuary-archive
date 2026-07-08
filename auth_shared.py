98d2dfef3a6132faed2fd1ddc38675def9d90bac
Your Name <you@example.com>
2026-07-08 19:34:18 -0400

    login bug

---
+++ auth_shared.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+
+def get_authenticator(credentials):
+    if "authenticator" not in st.session_state:
+        st.session_state["authenticator"] = stauth.Authenticate(
+            credentials,
+            st.secrets["cookie"]["name"],
+            st.secrets["cookie"]["key"],
+            st.secrets["cookie"]["expiry_days"]
+        )
+    return st.session_state["authenticator"]
