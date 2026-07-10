84f93b253f6ba943d188dd9e2cb0c6444153636f
Your Name <you@example.com>
2026-07-10 15:08:11 -0400

    chasing cookie bug

--- auth_shared.py
+++ auth_shared.py
@@ -4,7 +4,7 @@ import streamlit_authenticator as stauth
 def get_authenticator(credentials):
     if "authenticator" not in st.session_state:
         st.session_state["authenticator"] = stauth.Authenticate(
-            credentials["usernames"],
+            credentials,
             st.secrets["cookie"]["name"],
             st.secrets["cookie"]["key"],
             st.secrets["cookie"]["expiry_days"]
