13a51335c15827057091afe3011a12d3e37c8120
Your Name <you@example.com>
2026-09-09 07:51:21 -0400

    playlist bar

--- shared.py
+++ shared.py
@@ -627,7 +627,7 @@ def dank_playlist_player(show_label, tracks):
 
     loadTrack(0, false);
     </script>
-    """, height = 90 + min(len(tracks), 6) * 46 + 40 + 50)
+    """, height = 90 + min(len(tracks), 6) * 46 + 150)
 
 # -------------------------
 # MOBILE KEYBOARD SUPPRESSION FOR SELECTBOX
